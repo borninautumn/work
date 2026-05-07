794afd0369162aeefa53a92ebffdb690a441c6f8
autumn <60861324+borninautumn@users.noreply.github.com>
2026-05-07 14:54:19 +0900

    Add files via upload

---
+++ JSTYLE/모듈화(★)/REDIRECT.js
@@ -0,0 +1,4 @@
+// 리다이렉트
+document.addEventListener("DOMContentLoaded", function(){
+    window.location.href = 'https://www.evellet.com/board/board.html?code=jstyle07_image16&type=v&num1=999989&num2=00000&lock=N';
+});
